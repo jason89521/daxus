51729d729c59ee0d54d344ff0f6f28d4e4b987eb
Xuan <97ssps30212@gmail.com>
2023-05-23 19:23:59 +0800

    feat: return error for useFetch and useInfiniteFetch

--- src/lib/hooks/useInfiniteFetch.ts
+++ src/lib/hooks/useInfiniteFetch.ts
@@ -3,13 +3,13 @@ import type { InfiniteModelAccessor } from '../model';
 import { useModelAccessor } from './useModelAccessor';
 import type { FetchOptions } from './types';
 
-export function useInfiniteFetch<M, Arg, RD, D = any>(
-  accessor: InfiniteModelAccessor<M, Arg, RD>,
+export function useInfiniteFetch<M, Arg, RD, D = any, E = unknown>(
+  accessor: InfiniteModelAccessor<M, Arg, RD, E>,
   getSnapshot: (model: M) => D,
-  options: FetchOptions = {}
+  options: FetchOptions<D> = {}
 ) {
   const { stateDeps, status, data } = useModelAccessor(accessor, getSnapshot, options);
-  const { isFetching } = status;
+  const { isFetching, error } = status;
 
   const fetchNextPage = useCallback(() => {
     accessor.fetchNext();
@@ -23,6 +23,10 @@ export function useInfiniteFetch<M, Arg, RD, D = any>(
     get isFetching() {
       stateDeps.isFetching = true;
       return isFetching;
+    },
+    get error() {
+      stateDeps.error = true;
+      return error;
     },
   };
 }
