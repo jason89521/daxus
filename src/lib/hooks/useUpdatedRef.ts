85653781940be375a99eb601018bf8350e1c07af
Xuan <97ssps30212@gmail.com>
2023-05-28 23:08:32 +0800

    fix: update ref in useEffect

--- src/lib/hooks/useUpdatedRef.ts
+++ src/lib/hooks/useUpdatedRef.ts
@@ -1,8 +1,11 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function useUpdatedRef<T>(value: T) {
   const ref = useRef(value);
-  ref.current = value;
+
+  useEffect(() => {
+    ref.current = value;
+  });
 
   return ref;
 }
