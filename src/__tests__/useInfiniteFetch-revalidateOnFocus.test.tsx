17d89dc03531399d6082176002ca0aa97a4581f4
Xuan <97ssps30212@gmail.com>
2023-05-29 17:03:36 +0800

    test: add revalidate and fetching next page testing

--- src/__tests__/useInfiniteFetch-revalidateOnFocus.test.tsx
+++ src/__tests__/useInfiniteFetch-revalidateOnFocus.test.tsx
@@ -1,6 +1,6 @@
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import { act, fireEvent, screen } from '@testing-library/react';
 import { useInfiniteFetch } from '../lib';
-import { createPostModel, getPostModelControl, sleep } from './utils';
+import { createPostModel, getPostModelControl, sleep, render } from './utils';
 
 describe('useInfiniteFetch revalidateOnFocus', () => {
   test('should revalidate when window get focused', async () => {
@@ -56,5 +56,30 @@ describe('useInfiniteFetch revalidateOnFocus', () => {
     screen.getByText('items: focus title0');
     fireEvent.click(screen.getByText('next'));
     await screen.findByText('items: focus title0 title1');
+  });
+
+  test('should fetch next page if revalidation and fetching next page are triggered concurrently', async () => {
+    const control = getPostModelControl({});
+    const { getPostList, postAdapter } = createPostModel(control);
+    function Page() {
+      const { data, fetchNextPage } = useInfiniteFetch(getPostList(), model =>
+        postAdapter.readPagination(model, '')
+      );
+
+      return (
+        <div>
+          items: {data?.items.map(item => item.title).join(' ')}
+          <button onClick={() => fetchNextPage()}>next</button>
+        </div>
+      );
+    }
+
+    render(<Page />);
+    screen.getByText('items:');
+    await screen.findByText('items: title0');
+    act(() => (control.titlePrefix = 'next'));
+    fireEvent.focus(window);
+    fireEvent.click(screen.getByText('next'));
+    await screen.findByText('items: title0 next title1');
   });
 });
