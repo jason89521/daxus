5eab06efca08e727d9b4fb11a4e933b202a95391
Xuan <97ssps30212@gmail.com>
2023-06-28 18:10:15 +0800

    chore: specify export items explicitly

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,16 @@
 import type { AccessorOptions, UseAccessorReturn } from './hooks/types';
+import type { NormalAccessor, InfiniteAccessor } from './model';
+import type { AccessorOptionsProviderProps } from './contexts';
+import { createModel } from './model';
+import { useAccessor, useHydrate } from './hooks';
+import { createPaginationAdapter } from './adapters';
+import { AccessorOptionsProvider } from './contexts';
 
-export type { AccessorOptions, UseAccessorReturn };
-
-export * from './hooks';
-export * from './model';
-export * from './adapters';
-export * from './contexts';
+export type {
+  AccessorOptions,
+  UseAccessorReturn,
+  NormalAccessor,
+  InfiniteAccessor,
+  AccessorOptionsProviderProps,
+};
+export { createModel, useAccessor, useHydrate, createPaginationAdapter, AccessorOptionsProvider };
