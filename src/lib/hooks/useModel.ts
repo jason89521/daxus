9ab35ef7e06972ea41ba2f928363a37bee5e5d73
Xuan <97ssps30212@gmail.com>
2023-07-05 18:47:13 +0800

    docs: add typedoc for useModel

--- src/lib/hooks/useModel.ts
+++ src/lib/hooks/useModel.ts
@@ -3,6 +3,15 @@ import type { Model } from '../model/index.js';
 import { useServerStateKeyContext } from '../index.js';
 import { stableHash } from '../utils/index.js';
 
+/**
+ * This hook is useful when you only want to subscribe the state of a model, but don't want to use any accessor to trigger a request.
+ *
+ * @param model The model you want to subscribe
+ * @param getSnapshot
+ * Return the data you want whenever the state of the model changes.
+ * Make sure that this function is wrapped by `useCallback`. Otherwise it would cause unnecessary rerender.
+ * @returns The return value of the `getSnapshot` function.
+ */
 export function useModel<S extends object, SS>(model: Model<S>, getSnapshot: (state: S) => SS): SS {
   const serverStateKey = useServerStateKeyContext();
   const [subscribe, getData] = useMemo(() => {
