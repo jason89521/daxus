4a58c38f30fe08ba035a376bbf504725bfdb0a2f
Xuan <97ssps30212@gmail.com>
2023-08-08 15:18:39 +0800

    fix: remove provider.tsx

--- examples/simple-forum/app/layout.tsx
+++ examples/simple-forum/app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { Inter } from 'next/font/google';
-import Provider from './Provider';
 
 import { type ReactNode } from 'react';
 import Link from 'next/link';
@@ -15,15 +14,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>
-          <nav style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
-            <Link href="/">Home</Link>
-            <Link href="/forumId_1">Forum1</Link>
-            <Link href="/forumId_2">Forum2</Link>
-            <Link href="/forumId_3">Forum3</Link>
-          </nav>
-          {children}
-        </Provider>
+        <nav style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+          <Link href="/">Home</Link>
+          <Link href="/forumId_1">Forum1</Link>
+          <Link href="/forumId_2">Forum2</Link>
+          <Link href="/forumId_3">Forum3</Link>
+        </nav>
+        {children}
       </body>
     </html>
   );
