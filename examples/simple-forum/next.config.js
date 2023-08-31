090c54071b11e79565bbd65491d8939952fd5018
Xuan <97ssps30212@gmail.com>
2023-08-31 14:53:50 +0800

    add sw.js

--- examples/simple-forum/next.config.js
+++ examples/simple-forum/next.config.js
@@ -3,6 +3,10 @@ const { FileListPlugin } = require('./plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
+    (async () => {
+      const e = await config.entry();
+      console.log(e);
+    })();
     config.plugins.push(new FileListPlugin());
 
     return config;
